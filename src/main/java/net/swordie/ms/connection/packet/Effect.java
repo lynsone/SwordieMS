20e966c67826c1a5ebbf5f6dece05989a62119ed
AsuraRevenge <30930993+AsuraRevenge@users.noreply.github.com>
2018-06-11 22:53:16 +0200

    Added more effect methods to Effect

--- src/main/java/net/swordie/ms/connection/packet/Effect.java
+++ src/main/java/net/swordie/ms/connection/packet/Effect.java
@@ -44,12 +44,48 @@ public class Effect {
                 outPacket.encodeInt(getArg9()); // Leave type?
                 outPacket.encodeInt(getArg10()); // Type
                 break;
-            case ResetOnStateForOnOffSkill:
-                outPacket.encodeByte(getArg1()); //Boolean: Set Skill ON or OFF  state
-                break;
             case FieldExpItemConsumed:
                 outPacket.encodeInt(getArg1()); // Exp Gained
                 break;
+            case SkillMode:
+                outPacket.encodeInt(getArg1()); // Skill ID
+                outPacket.encodeInt(getArg2()); // Rotate (?)
+                outPacket.encodeInt(getArg3()); // Skip Frame (?)
+                break;
+            case RobbinsBomb:
+                outPacket.encodeByte(getArg1()); // Reset/Delete
+                outPacket.encodeInt(getArg2()); // BombCount
+                outPacket.encodeByte(getArg3()); // number (unknown)
+                break;
+            case PetBuff:
+            case ResetOnStateForOnOffSkill:
+            case SoulStoneUse:
+            case ItemLevelUp:
+            case ExpItemConsumed:
+            case QuestComplete:
+            case JobChanged:
+            case BuffItemEffect:
+            case Resist:
+            case LevelUp:
+                break;
+            case LeftMonsterNumber:
+                outPacket.encodeInt(getArg1()); // Number on Arrow
+                break;
+            case JewelCraft:
+                outPacket.encodeByte(getArg1()); // Result  0,2 = Success,     5 = Unk error,      Other = Fail
+                outPacket.encodeInt(getArg2()); // Item ID
+                break;
+            case AswanSiegeAttack:
+                outPacket.encodeByte(getArg1()); // 0 = Red Colour,     1 = Orange Colour
+                break;
+            case BlindEffect:
+                outPacket.encodeByte(getArg1()); //  true/false
+                break;
+            case ItemMaker:
+                outPacket.encodeInt(getArg1());  // success or failure      0 = Success,  1 = Failure
+                break;
+            case IncDecHPEffect:
+                outPacket.encodeByte(getArg1()); // amount being healed     0 = Miss
         }
     }
 
@@ -58,8 +94,8 @@ public class Effect {
                                                Position boxPos, int align, int lineSpace, TextEffectType type,
                                                int enterType, int leaveType) {
         Effect effect = new Effect();
-
         effect.setUserEffectType(TextEffect);
+
         effect.setString(msg);
         effect.setArg1(letterDelay);
         effect.setArg2(showTime);
@@ -75,16 +111,14 @@ public class Effect {
         return effect;
     }
 
-    public static Effect createABRechargeEffect() {
+    public static Effect createABRechargeEffect() { // Angelic Buster's Recharge userEffect
         Effect effect = new Effect();
         effect.setUserEffectType(ResetOnStateForOnOffSkill);
-
-        effect.setArg1(1);
 
         return effect;
     }
 
-    public static Effect fieldItemConsumed(int expGained) {
+    public static Effect fieldItemConsumed(int expGained) { // [exp]EXP+  effect from looting ExpOrbs
         Effect effect = new Effect();
         effect.setUserEffectType(FieldExpItemConsumed);
 
@@ -92,6 +126,149 @@ public class Effect {
 
         return effect;
     }
+
+    public static Effect skillModeEffect(int skillID, int mode, int modeStatus) { // Unknown Effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(SkillMode);
+
+        effect.setArg1(skillID);
+        effect.setArg2(mode); //rotate
+        effect.setArg3(modeStatus); //skip frame
+
+        return effect;
+    }
+
+    public static Effect robbinsBombEffect(boolean reset, int bombCount, byte number) { //Displays bomb with [bombCount] on the Bomb, above user,   Unsure what '[int] number' does
+        Effect effect = new Effect();
+        effect.setUserEffectType(RobbinsBomb);
+
+        effect.setArg1(reset ? 1 : 0);  // if true, resets/delets bombs
+        effect.setArg2(bombCount);      // Number displayed on the Bomb
+        effect.setArg3(number);         // unknown
+
+        return effect;
+    }
+
+    public static Effect lefMonsterNumberEffect(int count) { //Displays arrow pointing towards the user, with [count] on the arrow,  Maximum count is 5
+        Effect effect = new Effect();
+        effect.setUserEffectType(LeftMonsterNumber);
+
+        effect.setArg1(count); // number/counter on the arrow
+
+        return effect;
+    }
+
+    public static Effect petBuffEffect() { //Displays the PetBuff Effect onto the user
+        Effect effect = new Effect();
+        effect.setUserEffectType(PetBuff);
+
+        return effect;
+    }
+
+    public static Effect jewelCraftResultEffect(byte result, int itemID) { // result = Displays Success/Fail effect above player head with smile/frown respectively, itemID will show itemName in chat
+        Effect effect = new Effect();
+        effect.setUserEffectType(JewelCraft);
+
+        effect.setArg1(result); // 0, 2  = Success/Smile,       5 = Request denied due to unk error,        Everything else = Fail/Frown
+        effect.setArg2(itemID);
+
+        return effect;
+    }
+
+    public static Effect azwanSpearEffect(byte colour) { // 0 = red,    1 = orange
+        Effect effect = new Effect();
+        effect.setUserEffectType(AswanSiegeAttack);
+
+        effect.setArg1(colour);
+
+        return effect;
+    }
+
+    public static Effect blindEffect(boolean active) {  // gives User the Blind Status Effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(BlindEffect);
+
+        effect.setArg1(active ? 1 : 0);
+
+        return effect;
+    }
+
+    public static Effect soulStoneUseEffect() { // gives the SoulStone Used Chat
+        Effect effect = new Effect();
+        effect.setUserEffectType(SoulStoneUse);
+
+        return effect;
+    }
+
+    public static Effect expItemConsumedEffect() { // gives exp item consumption effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(ExpItemConsumed);
+
+        return effect;
+    }
+
+    public static Effect itemMakerEffect(boolean success) { // displays itemMaker result
+        Effect effect = new Effect();
+        effect.setUserEffectType(ItemMaker);
+
+        effect.setArg1(success ? 0 : 1); // 0 = Success,    1 = Failure
+
+        return effect;
+    }
+
+    public static Effect itemLevelUpEffect() { //displays the Equipment Level Up effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(ItemLevelUp);
+
+        return effect;
+    }
+
+    public static Effect questCompleteEffect() { //displays the Quest Complete effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(QuestComplete);
+
+        return effect;
+    }
+
+    public static Effect incDecHPEffect(byte amount) { //displays the HP healing  effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(ItemLevelUp);
+
+        effect.setArg1(amount); // amount shown being healed,  0 = miss
+
+        return effect;
+    }
+
+    public static Effect changeJobEffect() { //displays the JobAdvancement Effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(JobChanged);
+
+        return effect;
+    }
+
+    public static Effect buffItemEffect() { //displays the Buff Item Effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(BuffItemEffect);
+
+        return effect;
+    }
+
+    public static Effect resistDamageEffect() { //displays the "Resist" Hit
+        Effect effect = new Effect();
+        effect.setUserEffectType(Resist);
+
+        return effect;
+    }
+
+    public static Effect levelUpEffect() { //displays the Level Up  Effect
+        Effect effect = new Effect();
+        effect.setUserEffectType(LevelUp);
+
+        return effect;
+    }
+
+
+
 
 
     public void setUserEffectType(UserEffectType userEffectType) {
