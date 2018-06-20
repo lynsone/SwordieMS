a2a3edd952b50d483dc6ab7df00055ab7ae8000a
Tim <tim.blokk@gmail.com>
2018-06-20 18:51:34 +0200

    Added default gains for thief job advancement, and made sure that the AP from the beginner job is refunded.

--- scripts/quest/q1404e.py
+++ scripts/quest/q1404e.py
@@ -1,4 +1,5 @@
 #   Thieves of Kerning City
+from net.swordie.ms.enums import Stat
 
 def init():
     sm.sendAskYesNo("So you want to become a sneaky Thief?")
@@ -7,7 +8,11 @@ def action(response, answer):
     if response == 1:
         sm.completeQuestNoRewards(parentID)
         sm.jobAdvance(400) #Thief
+        sm.addAP(40)
+        sm.getChr().addStatAndSendPacket(Stat.str, -40)
         sm.sendSayOkay("You are now a #bThief#k.")
-
+        sm.giveItem(2070000, 500)
+        sm.giveItem(1472000, 1)
+        sm.giveItem(1332007, 1)
         sm.startQuestNoCheck(1421)
     sm.dispose()
