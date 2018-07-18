fc79422858d95adcd12081b053c443cbba1440b7
AsuraRevenge <30930993+AsuraRevenge@users.noreply.github.com>
2018-07-18 21:45:39 +0200

    Forgot 1 NPC script, fixed strawDummy Constant convention

--- scripts/item/consume_2434325.py
+++ scripts/item/consume_2434325.py
@@ -1,6 +1,7 @@
 #Scarecrow Summoning Sack (2434325) | Used to spawn a Dojo Dummy on Client's position
 from net.swordie.ms.constants import GameConstants
-strawDummy = 9305655
+
+STRAW_DUMMY_ID = 9305655
 
 def init():
     if not sm.getChr().getField().getReturnMap() == sm.getFieldID():
@@ -10,8 +11,8 @@ def init():
         sm.chat("You cannot spawn a dummy whilst there are other monsters or dummies in the map")
 
     else:
-        sm.spawnMobOnChar(strawDummy)
-        sm.removeMobsAfterTimer(strawDummy, GameConstants.DOJO_DUMMY_DURATION * 60) #Template ID & Seconds
+        sm.spawnMobOnChar(STRAW_DUMMY_ID)
+        sm.removeMobsAfterTimer(STRAW_DUMMY_ID, GameConstants.DOJO_DUMMY_DURATION * 60) #Template ID & Seconds
         sm.chatBlue("The Training Dummy will be removed after "+ str(GameConstants.DOJO_DUMMY_DURATION) +" minutes.")
         sm.consumeItem(parentID)
     sm.dispose()
---
+++ scripts/npc/9270021.py
@@ -0,0 +1,4 @@
+#Wendy (9270021) | CBD
+def init():
+    sm.openShop(parentID)
+    sm.dispose()
