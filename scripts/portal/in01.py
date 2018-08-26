61dac2cdd53817f979bffab1306432577b23d370
AsuraRevenge <30930993+AsuraRevenge@users.noreply.github.com>
2018-08-26 20:17:24 +0200

    Typo in root00,  added more exceptions to the in01 script

--- scripts/portal/in01.py
+++ scripts/portal/in01.py
@@ -100,7 +100,7 @@ else:
         fieldID = sm.getFieldID()
 
         if fieldID == 130020000:  # Entrance to Drill Hall
-            if not sm.hasQuest(20873):  # WA 2nd job quest
+            if not sm.hasQuest(20871) and not sm.hasQuest(20872) and not sm.hasQuest(20873) and not sm.hasQuest(20874) and not sm.hasQuest(20875):  # KoC 2nd JobAdv Quests
                 sm.chat("Sorry, only knights looking to job advance to the second job may enter here.")
                 warp = False
 
--- scripts/portal/root00.py
+++ scripts/portal/root00.py
@@ -3,5 +3,5 @@ def init():
     if sm.hasQuest(30000) or sm.hasQuestCompleted(30000): # Root Abyss quest -  [Root Abyss] An Urgent Summons
         sm.warp(105010200, 0) # Secret Swamp
     else:
-        sm.chat("There path is blocked by a thick fog.")
+        sm.chat("The path is blocked by a thick fog.")
     sm.dispose()
