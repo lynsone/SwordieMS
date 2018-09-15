67a77e39a24abd4a5a2c10a2ca0500e039b84918
Ephenia <ephenia9@gmail.com>
2018-09-15 17:10:42 -0400

    you didn't see that

--- scripts/portal/101070000_up.py
+++ scripts/portal/101070000_up.py
@@ -3,11 +3,8 @@
 FANZY = 1040002 # NPC ID
 MIDSUMMER_NIGHTS_FOREST_PATH_FROM_ELLINEL = 101074001 # MAP ID
 
-def init():
-    sm.setSpeakerID(FANZY)
-    sm.sendAskYesNo("Head back to #b North Forest: Giant Tree#k?")
+sm.setSpeakerID(FANZY)
 
-def action(response, answer):
-    if response == 1:
-        sm.warp(MIDSUMMER_NIGHTS_FOREST_PATH_FROM_ELLINEL)
-    sm.dispose()
+response = sm.sendAskYesNo("Head back to #bNorth Forest: Giant Tree#k?")
+if response == 1:
+    sm.warp(MIDSUMMER_NIGHTS_FOREST_PATH_FROM_ELLINEL)
