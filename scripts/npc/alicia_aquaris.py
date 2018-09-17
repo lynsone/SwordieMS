6d123f63f7b466f7cb5d9794a45e165d58c8475b
Siphonic <vantropus@gmail.com>
2018-09-18 01:42:11 +0800

    Never fucked up so hard in my life :pepehands:

--- scripts/npc/alicia_aquaris.py
+++ scripts/npc/alicia_aquaris.py
@@ -1,7 +1,7 @@
 # Alicia's Soul | Tower of Oz (992000000)
 CLEVER_LADY_HOTLINE = 2432461
 
-answer = sm.sendSay("How may I help you, little one?\r\n\r\n#b"
+answer = sm.sendNext("How may I help you, little one?\r\n\r\n#b"
                     "#L0#I want to expand my Stupid Pill slots.#l\r\n"
                     "#L1#I lost my Clever Lady Hotline.#l\r\n"
                     "#L2#I don't need any more guidance from you.#l")
@@ -13,13 +13,12 @@ elif answer == 1:
     if sm.hasItem(CLEVER_LADY_HOTLINE):
         sm.sendSayOkay("Sorry, you already have a #bClever Lady Hotline.")
 
-    else:
-        if sm.canHold(CLEVER_LADY_HOTLINE):
-            sm.giveItem(CLEVER_LADY_HOTLINE)
-            sm.sendSayOkay("I've given you a new #bClever Lady Hotline#k. Please try not to lose it again.")
+    elif not sm.canHold(CLEVER_LADY_HOTLINE):
+        sm.sendSayOkay("Please make sure you have enough inventory space")
 
-        else:
-            sm.sendSayOkay("Please make sure you have space in your inventory.")
+    else:
+        sm.giveItem(CLEVER_LADY_HOTLINE)
+        sm.sendSayOkay("I've given you a new #bClever Lady Hotline#k. Please try not to lose it again.")
 
 else:
     sm.sendSayOkay("Do come to me again if you need any guidance.")
