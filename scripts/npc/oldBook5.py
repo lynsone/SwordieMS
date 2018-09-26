96dc8ccc9231aeee9a48cc1cc3ec9e2594042d56
Tiger <shabbar.haider@outlook.com>
2018-09-25 22:20:44 -0400

    naming convention.

--- scripts/npc/oldBook5.py
+++ scripts/npc/oldBook5.py
@@ -2,10 +2,10 @@
  # The Book of Ancient is Back (Quest 3034)
  # Author: Tiger
 
-COST = 500000
+mesos = 500000
 
 if sm.hasQuestCompleted(3034): # The Book of Ancient is Back
-    response = sm.sendAskYesNo("You've been so much of a help to me... If you have any #t" + str(4004004) + "#, I can refine it for you for only #b" + str(COST) + " meso#k each.")
+    response = sm.sendAskYesNo("You've been so much of a help to me... If you have any #t" + str(4004004) + "#, I can refine it for you for only #b" + str(mesos) + " meso#k each.")
 
     if response:
         answer = sm.sendAskNumber("Okay, so how many do you want me to make?", 1, 1, 100)
@@ -13,7 +13,7 @@ if sm.hasQuestCompleted(3034): # The Book of Ancient is Back
         if not sm.canHold(4005004): # Not enough space
             sm.sendSayOkay("Please make some more space in your inventory.")
 
-        elif sm.getMesos() < answer * COST: # Too little mesos
+        elif sm.getMesos() < answer * mesos: # Too little mesos
             sm.sendSayOkay("I'm sorry, but I am NOT doing this for free.")
 
         elif not sm.hasItem(4004004, answer * 10): # Does not have enough Dark Crystal Ores, TODO: check if this looks right (for 5 DC you need 50 DCO?)
@@ -22,7 +22,7 @@ if sm.hasQuestCompleted(3034): # The Book of Ancient is Back
         else:   # can afford & can hold
             sm.consumeItem(4004004, answer * 10)
             sm.giveItem(4005004, answer)
-            sm.deductMesos(answer * COST)
+            sm.deductMesos(answer * mesos)
             sm.sendSayOkay("Use it wisely.")
 else:
     sm.sendSayOkay("Go away, I'm trying to meditate.")
