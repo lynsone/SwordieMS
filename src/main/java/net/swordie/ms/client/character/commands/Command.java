75f56685265c0aa0ce3765eab9024ac6a788031b
Tim <tim.blokk@gmail.com>
2018-10-11 22:32:24 +0200

    Fixed the command annotation not being saved on runtime.

--- src/main/java/net/swordie/ms/client/character/commands/Command.java
+++ src/main/java/net/swordie/ms/client/character/commands/Command.java
@@ -2,6 +2,10 @@ package net.swordie.ms.client.character.commands;
 
 import net.swordie.ms.enums.AccountType;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Command {
 
     String[] names();
