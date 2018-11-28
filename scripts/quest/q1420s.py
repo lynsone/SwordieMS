0c754ab291b2cbb7d5045c1d14d68d5cb4f643e3
Tim <tim.blokk@gmail.com>
2018-11-28 17:10:38 +0100

    Fixed some typos + 5th path quest. Changed drop rate for marble quest items to 90%.

--- scripts/quest/q1420s.py
+++ scripts/quest/q1420s.py
@@ -4,20 +4,21 @@ darkMarble = 4031013
 job = "Crossbowman"
 monster = "Stone Golem"
 
-sm.sendNext("You wish to become a #b"+ job +"#k?\r\n"
-                                                "a #b"+ job +"#k is specialised in short range attacks and use #bdaggers#k to defeat their enemies. "
-                                                             "There are many useful skills you can acquire.")
+sm.sendNext("You wish to become a #b" + job + "#k?\r\n"
+                  "A #b" + job + "#k is specialised in short range attacks and use #bdaggers#k to defeat their enemies. "
+                  "There are many useful skills you can acquire.")
 
+sm.sendNext("Before I teach you the ways of the " + job + ", you will have to accomplish a very difficult test. "
+                  "I will warp you into a special map, in which I require you to defeat #b" + monster + "#k "
+                  "and return 30 #i" + str(
+    darkMarble) + "##z" + str(darkMarble) + "#s to me.")
 
-sm.sendNext("Before I teach you the ways of the "+ job +", you will have to accomplish a very difficult test. "
-            "I will warp you into a special map, in which I require you to defeat #b"+ monster +"#k "
-            "and return 30 #i"+ str(darkMarble) +"##z"+ str(darkMarble) +"#s to me.")
-
-response = sm.sendAskYesNo("Once you enter the map, you #rcannot#k return without the #b#t"+ str(darkMarble) +"#s#k, if you die you will lose your experience.\r\n"
-                           "Are you ready?")
+response = sm.sendAskYesNo("Once you enter the map, you #rcannot#k return without the #b#t" + str(
+    darkMarble) + "#s#k, if you die you will lose your experience.\r\n"
+                  "Are you ready?")
 
 if response:
-    sm.warp(910070000, 0) # Archer Test Site
+    sm.warp(910070000, 0)  # Archer Test Site
     sm.startQuestNoCheck(parentID)
 else:
     sm.sendSayOkay("You cannot be an Archer forever. You #bwill#k have to face up to the test.\r\n"
