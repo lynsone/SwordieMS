29ebc175a056825819b0323fd1870258ce08ff5a
shavit <the@shav.it>
2019-02-21 18:23:45 +0200

    Added tutorial skips.
    * Made Cygnus tutorial doable.
    * Made Explorer tutorial skippable.
    * Made Cygnus tutorial skippable.
    * Fixed missing flash jump skill for Noblesse.

--- scripts/field/Advanture_tuto11.py
+++ scripts/field/Advanture_tuto11.py
@@ -1,16 +1,41 @@
-# Maple Road : Maple Tree Hill (4000011)  Map for Advanture Tutorial
+# Maple Road : Maple Tree Hill (4000011)  Map for Explorer Tutorial
 
-# NPC name - "???"
-UNK_NPC = 10300
+UNK_NPC = 10300 # NPC name - "???"
 
-VELLUM_STONE = 1064028
-if sm.hasQuest(32202):
+# Tutorial skipper snippet
+def skip_tutorial():
+    MAPLE_ADMINISTARTOR = 2007
+
+    quests_to_complete = [
+        32202, # Mystical Maple Tree
+        32203  # The New Explorer
+    ]
+
+    map_to_warp = 104000000 # Lith Harbor
+    target_level = 10
+
+    sm.setSpeakerID(MAPLE_ADMINISTARTOR)
+    sm.removeEscapeButton()
+    sm.lockInGameUI(True)
+
+    if sm.sendAskYesNo("Would you like to skip the tutorial questline and instantly arrive at #m" + str(map_to_warp) + "#?\r\n#rNote#k: You will not receive starting items."):
+        if sm.getChr().getLevel() < target_level:
+            sm.addLevel(target_level - sm.getChr().getLevel())
+
+        for quest in quests_to_complete:
+            sm.startQuestNoCheck(quest)
+            sm.completeQuestNoRewards(quest)
+        
+        sm.warp(map_to_warp)
+        
+    sm.lockInGameUI(False)
     sm.dispose()
-else:
+
+if not sm.hasQuest(32202) or not sm.hasQuestCompleted(32203):
+    skip_tutorial()
     sm.lockInGameUI(True)
     sm.showFieldEffect("maplemap/enter/10000", 0)
     sm.sendDelay(1000)
-
 
     sm.spawnNpc(UNK_NPC, -240, 220)
     sm.showNpcSpecialActionByTemplateId(UNK_NPC, "summon", 0)
