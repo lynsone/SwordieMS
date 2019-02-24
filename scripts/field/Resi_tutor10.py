29040c0aa85c9b8a9edc76c4b85ef1dc895a8e3c
shavit <the@shav.it>
2019-02-24 13:34:13 +0200

    Removed misleading text in intro skip dialogs.

--- scripts/field/Resi_tutor10.py
+++ scripts/field/Resi_tutor10.py
@@ -19,7 +19,7 @@ def skip_tutorial():
 	sm.removeEscapeButton()
 	sm.lockInGameUI(True)
 
-	if sm.sendAskYesNo("Would you like to skip the tutorial questline and instantly arrive at #m" + str(map_to_warp) + "#?\r\n#rNote#k: You will not receive starting items."):
+	if sm.sendAskYesNo("Would you like to skip the tutorial questline and instantly arrive at #m" + str(map_to_warp) + "#?"):
 		if sm.getChr().getLevel() < target_level:
 			sm.addLevel(target_level - sm.getChr().getLevel())
 
