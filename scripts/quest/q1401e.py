522f3834d0cce37ede496cdf8ba9e59c1a6a00c3
g1zstar <sensiblemagic@gmail.com>
2019-07-21 01:37:04 -0400

    1st job advancements now correctly don't give AP, now reset all AP just to keep it simple. Bowman 1st Job Advance now gives beginner bow and arrows Thief 1st Job Advance now gives specific beginner equips instead of generic level 10 Pirate 1st Job Advance now gives beginner weapons and bullets

--- scripts/quest/q1401e.py
+++ scripts/quest/q1401e.py
@@ -1,14 +1,13 @@
 # 1401 - Warriors of Perion
 
-sm.setSpeakerID(10202) # Dances with Balrog
+sm.setSpeakerID(1022000) # Dances with Balrog
 response = sm.sendAskYesNo("So you want to become a Warrior?")
 
 if response:
     sm.completeQuestNoRewards(parentID)
     sm.setJob(100)
-    sm.setSp(5)
-    sm.setAp(28)
-    sm.sendSayOkay("You are now a #bWarrior#k.")
+    sm.setSP(5)
+    sm.resetAP(False)
     sm.giveItem(1302182)
-    sm.completeQuest(parentID)
+    sm.sendSayOkay("You are now a #bWarrior#k.")
 sm.dispose()
--- scripts/quest/q1402e.py
+++ scripts/quest/q1402e.py
@@ -1,15 +1,13 @@
 # 1402 - Magicians of Ellinia
 
-sm.setSpeakerID(9000025) # Grendel the Really Old
+sm.setSpeakerID(1032001) # Grendel the Really Old
 response = sm.sendAskYesNo("So you want to become a Magician?")
 
 if response:
     sm.completeQuestNoRewards(parentID)
     sm.setJob(200) # Magician
-    sm.addSP(5)
-    sm.addAP(5)
-    sm.getChr().addStatAndSendPacket(Stat.str, -40)
-    sm.getChr().addStatAndSendPacket(Stat.inte, 40)
+    sm.setSP(5)
+    sm.resetAP(False)
     sm.giveItem(1372043)
     sm.sendSayOkay("You are now a #bMagician#k.")
 sm.dispose()
