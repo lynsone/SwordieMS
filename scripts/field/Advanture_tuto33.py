67c9d6334f2c02744ccfef413c1ac57a94b761ee
g1zstar <sensiblemagic@gmail.com>
2019-07-21 01:34:18 -0400

    minor changes/fix

--- scripts/field/Advanture_tuto33.py
+++ scripts/field/Advanture_tuto33.py
@@ -2,6 +2,6 @@
 if not sm.hasMobsInField():
     sm.spawnMob(9300815, -152, 150, False) # Spawn Mano
 else:
-    sm.killmobs()
+    sm.killMobs()
     sm.spawnMob(9300815, -152, 150, False) # Spawn Mano
 sm.dispose()
