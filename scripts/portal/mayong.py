7dc604ad10150600bee3735f5b76e68b40672f72
g1zstar <sensiblemagic@gmail.com>
2019-07-22 15:41:46 -0400

    order of check -> action, fix

--- scripts/portal/mayong.py
+++ scripts/portal/mayong.py
@@ -1,10 +1,10 @@
 # Portal to Manon
 if sm.hasQuest(1451) or sm.hasQuest(1453) or sm.hasQuest(1455) or sm.hasQuest(1457) or sm.hasQuest(1459):
     if sm.checkParty():
-        sm.clearPartyInfo(924000200)
         if sm.getPartySize() > 1:
             sm.chat("Please be in a party of one.")
             sm.dispose()
+        sm.clearPartyInfo(924000200)
         sm.warpPartyIn(924000200)
     sm.dispose()
 sm.warp(240020401, 3)
